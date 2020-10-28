3689091e2da5de9fe3be3649d258a5e90de480c4
elijahbrookss <elijahcbrooks@gmail.com>
2020-10-28 10:03:48 -0500

    Done.

--- app.rb
+++ app.rb
@@ -2,5 +2,20 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+  get '/newteam' do
+    erb :newteam
+  end
+
+  post '/team' do
+    @hash = {
+      "coach": "Coach",
+      "pg": "Point Guard",
+      "sg": "Shooting Guard",
+      "sf": "Small Forward",
+      "pf": "Power Forward",
+      "c": "Center"
+    }
+    erb :team
+  end
 
 end
